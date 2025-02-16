accd2e3abcc94e47d67dde6c5cbf26c25a93ebe7
Nikhil <nikhil.4002.50.82@gmail.com>
2025-02-16 12:45:52 +0530

    fixed the issue

--- client/src/components/StatsCounter.jsx
+++ client/src/components/StatsCounter.jsx
@@ -60,7 +60,7 @@ const StatsCounter = () => {
     ];
 
     return (
-        <div className="w-full flex justify-center items-center">
+        <div ref={statsRef} className="w-full flex justify-center items-center">
             <div className="mt-[2em] w-[95%] bg-[#003cff] p-[1.5em] rounded-3xl md:rounded-[5em] shadow-lg flex flex-col md:flex-row justify-around items-center text-center space-y-6 md:space-y-0 md:space-x-6 ">
             {stats.map((stat, index) => (
                 <div key={index} className="w-1/4 border-r last:border-none border-white/50">
