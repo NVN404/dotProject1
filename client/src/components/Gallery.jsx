1dffb0778175e73f6e0ffdefd23a642ed9ecef13
vilas-naik <113898490+vilas-naik@users.noreply.github.com>
2025-02-27 00:35:52 +0530

    Made 5 columns for gallery

--- client/src/components/Gallery.jsx
+++ client/src/components/Gallery.jsx
@@ -7,9 +7,15 @@ const Gallery = () => {
     "/gal1.jpg",
     "/gal2.jpg",
     "/gal2.jpg",
+    "/gal2.jpg",
+    "/gal2.jpg",
+    "/gal3.jpg",
+    "/gal3.jpg",
     "/gal3.jpg",
     "/gal4.jpg",
     "/gal4.jpg",
+    "/gal5.jpg",
+    "/gal5.jpg",
     "/gal5.jpg",
     "/gal6.jpg",
     "/gal6.jpg",
@@ -20,29 +26,29 @@ const Gallery = () => {
       <HeaderForOthers />
 
       <div className="md:hidden flex justify-center items-center h-[80vh] w-full overflow-hidden relative mt-[7em] md:mt-[9em]">
-  <div className="absolute top-0 left-0 w-full h-[15%] bg-gradient-to-b from-black/40 to-transparent pointer-events-none z-10"></div>
-  <div className="absolute bottom-0 left-0 w-full h-[15%] bg-gradient-to-t from-black/40 to-transparent pointer-events-none z-10"></div>
-  <div className="relative h-screen w-[75vw] md:w-[60vw] lg:w-[60vw] overflow-hidden">
-    <div className="absolute top-0 flex flex-col items-center gap-4 animate-scroll-infinite">
-      {[...images, ...images].map((src, index) => (
-        <img
-          key={index}
-          src={src}
-          alt={`Gallery ${index + 1}`}
-          className="w-full rounded-md"
-        />
-      ))}
-    </div>
-  </div>
-</div>
+        <div className="absolute top-0 left-0 w-full h-[15%] bg-gradient-to-b from-black/40 to-transparent pointer-events-none z-10"></div>
+        <div className="absolute bottom-0 left-0 w-full h-[15%] bg-gradient-to-t from-black/40 to-transparent pointer-events-none z-10"></div>
+        <div className="relative h-screen w-[75vw] md:w-[60vw] lg:w-[60vw] overflow-hidden">
+          <div className="absolute top-0 flex flex-col items-center gap-4 animate-scroll-infinite">
+            {[...images, ...images].map((src, index) => (
+              <img
+                key={index}
+                src={src}
+                alt={`Gallery ${index + 1}`}
+                className="w-full rounded-md"
+              />
+            ))}
+          </div>
+        </div>
+      </div>
 
 
 
-      <div className=" hidden md:grid lg:grid sm:grid grid-cols-3 gap-4 p-4 max-w-4xl mx-auto mt-[7em] md:mt-[9em] ">
+      <div className="grid grid-cols-5 gap-4 p-4 w-full mx-auto mt-[7em] md:mt-[9em]">
         {images.map((img, index) => (
           <div
             key={index}
-            className={`relative overflow-hidden rounded-lg shadow-lg h-48 ${index % 3 === 1 ? 'mt-8' : ''
+            className={`relative overflow-hidden rounded-lg shadow-lg h-48 w-72 ${index % 5 === 1 || index % 5 === 3 ? 'mt-8' : index % 5 === 2 ? 'mt-4' : ''
               }`}
           >
             <img
