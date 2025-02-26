d3be66cd4e4f94284cf915bf6a0c5fc6c72e37ef
Nikhil <nikhil.4002.50.82@gmail.com>
2025-02-26 16:49:57 +0530

    added scroll to top

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Navigate } from "react-router-dom";
 
 import Footer from './components/Footer';
+import ScrollToTop from './components/ScrollToTop';
 
 import Home from './components/Home';
 import ContactUs from './components/ContactUs';
@@ -30,6 +31,7 @@ const App = () => {
       <AdmissionContext.Provider value={{ openAdmissionForm, setOpenAdmissionForm }}>
         {/* Wrap everything inside BrowserRouter */}
         <BrowserRouter>
+        <ScrollToTop />
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/contact" element={<ContactUs />} />
