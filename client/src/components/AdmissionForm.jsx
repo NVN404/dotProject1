3fcd81de45afc1a70de601b8d54cb2e93fc6aad8
Nikhil <nikhil.4002.50.82@gmail.com>
2025-02-28 20:49:21 +0530

    modified form

--- client/src/components/AdmissionForm.jsx
+++ client/src/components/AdmissionForm.jsx
@@ -55,8 +55,8 @@ const AdmissionForm = () => {
         <>
             {openAdmissionForm && (
                 <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
-                    <div ref={formRef} className="bg-[#2973B2] p-16 rounded-lg w-80 md:w-[30rem]">
-                        <h2 className="text-xl font-bold mb-4 text-center text-white">ADMISSION ENQUIRY</h2>
+                    <div ref={formRef} className="bg-[#2973B2] p-8 md:p-10 rounded-lg w-80 md:w-[30rem]">
+                        <h2 className="text-2xl md:text-3xl font-bold mb-8 text-center text-white">ADMISSION ENQUIRY</h2>
                         <form onSubmit={handleSubmit} className="space-y-6 text-black">
                             <input type="text" name="name" placeholder="Your Name" spellCheck="false" className="w-full p-2 border rounded-lg focus:outline-none" onChange={handleChange} required />
                             <input type="email" name="email" placeholder="Your Email" spellCheck="false" className="w-full p-2 border rounded-lg focus:outline-none" onChange={handleChange} required />
