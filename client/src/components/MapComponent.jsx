71051d643d9d65b775d74cfd1e3f37db3a446271
Nikhil <nikhil.4002.50.82@gmail.com>
2025-02-28 21:10:44 +0530

    added markers icon

--- client/src/components/MapComponent.jsx
+++ client/src/components/MapComponent.jsx
@@ -1,6 +1,18 @@
 import React, { useEffect, useRef } from "react";
 import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
+
+// Fix marker icon issue
+const customIcon = new L.Icon({
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+  iconSize: [25, 41], // Default Leaflet icon size
+  iconAnchor: [12, 41], // Ensures proper placement
+  popupAnchor: [1, -34], // Adjusts popup position
+});
 
 const AutoOpenPopup = ({ markerRef }) => {
   const map = useMap();
@@ -25,16 +37,17 @@ const MapComponent = () => {
       center={position}
       zoom={12}
       className="h-full w-full rounded-md shadow-md z-0"
+      style={{ height: "400px", width: "100%" }} // Ensures it renders properly
       scrollWheelZoom={false}  // Disable scroll zoom
       doubleClickZoom={false}  // Disable double-click zoom
-      touchZoom={true}  // Disable pinch zoom
+      touchZoom={true}  // Enable pinch zoom
       zoomControl={true} // Enable zoom control buttons
     >
       <TileLayer
         url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       />
-      <Marker position={position} ref={markerRef}>
+      <Marker position={position} ref={markerRef} icon={customIcon}>
         <Popup>📍 Dobbespet Public School</Popup>
       </Marker>
       <AutoOpenPopup markerRef={markerRef} />
