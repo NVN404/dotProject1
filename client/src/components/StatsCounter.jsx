8222e0dbd4b2e428c02d1add036a7014d52b4d29
Nikhil <nikhil.4002.50.82@gmail.com>
2025-02-28 23:29:40 +0530

    updated statsCounter

--- client/src/components/StatsCounter.jsx
+++ client/src/components/StatsCounter.jsx
@@ -55,7 +55,7 @@ const StatsCounter = () => {
     const stats = [
         { value: 120, label: "State Rank" ,src:"bangalore.png"},
         { value: 40, label: "Professional Lecturers",src:"teacher.png" },
-        { value: 4, label: "Years of History",src:"history.png" },
+        { value: 12, label: "Years of History",src:"history.png" },
         { value: 100, label: "Results Every Year",src:"result.png" },
     ];
 
