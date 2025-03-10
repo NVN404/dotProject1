36f42a649507fb68f9074788073f25dec78dbc27
Nikhil R Nambiar <nikhil.4002.50.82@gmail.com>
2025-03-10 21:12:03 +0530

    Update Panel.jsx

--- client/src/components/Panel.jsx
+++ client/src/components/Panel.jsx
@@ -7,7 +7,7 @@ const Panel = ({ src, content }) => {
       <img
         src={src}
         alt="Landscape"
-        className="w-full h-full object-cover rounded-lg opacity-80"
+        className="w-full h-full object-cover rounded-lg opacity-90"
       />
       <div className="w-full  sm:w-auto md:w-auto lg:w-auto absolute top-full transform -translate-y-1/2 p-6 md:p-10 bg-background shadow-2xl drop-shadow-lg ring-1 ring-gray-900/10 text-center">
         <span className="max-w-100 text-white text-4xl lg:text-5xl font-helvetica font-extrabold">
