895cfe5cc10e9deae5c02d5308881ed5ec535635
vilas-naik <113898490+vilas-naik@users.noreply.github.com>
2025-03-10 19:41:17 +0530

    trial

--- client/src/components/Server.jsx
+++ client/src/components/Server.jsx
@@ -1,8 +1,8 @@
 const sendMailTo = (subject, { name, email, phone, message }) => {
   const body = `Name: ${name}\nEmail: ${email}\nPhone: ${phone}\n\nMessage:\n${message}`;
-  const mailtoLink = `https://mail.google.com/mail/?view=cm&fs=1&to=nikhil.4002.50.82@gmail.com&su=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
-  
-  window.open(mailtoLink, "_blank");
+  const mailtoLink = `mailto:nikhil.4002.50.82@gmail.com?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
+
+  window.location.href = mailtoLink;
 };
 
 export default sendMailTo;
