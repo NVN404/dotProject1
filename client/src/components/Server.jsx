db4e0388d1db80e92335e4a3755d4d3239a28441
Nikhil <nikhil.4002.50.82@gmail.com>
2025-03-16 22:43:42 +0530

    added school email in server.jsx

--- client/src/components/Server.jsx
+++ client/src/components/Server.jsx
@@ -1,7 +1,7 @@
 const sendMailTo = (subject, { name, email, phone, message }) => {
   const body = `Name: ${name}\nEmail: ${email}\nPhone: ${phone}\n\nMessage:\n${message}`;
-  const mailtoLink = `mailto:nikhil.4002.50.82@gmail.com?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
-  const gmailLink = `https://mail.google.com/mail/?view=cm&fs=1&to=nikhil.4002.50.82@gmail.com&su=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
+  const mailtoLink = `mailto:dpslakkur2010@gmail.com?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
+  const gmailLink = `https://mail.google.com/mail/?view=cm&fs=1&to=dpslakkur2010@gmail.com&su=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
 
   const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
 
